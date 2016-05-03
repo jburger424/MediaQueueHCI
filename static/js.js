e6da65a4c9c8770176f4fb0b0548be7341a524cd
Jonathan Burger <jburger424@gmail.com>
2016-05-03 01:39:23 -0400

    cleaned .js file

--- static/js.js
+++ static/js.js
@@ -103,32 +103,7 @@ jQuery(function ($) { // First argument is the jQuery object
             });
         };
 
-
-        /*$("ul#playables").on('click', 'li', function () {
-         //alert($(this).text());
-         $("ul#playables .active").removeClass("active");
-         $(this).addClass("active");
-         player.loadVideoById({
-         'videoId': $(this).attr("data-url"),
-         'suggestedQuality': 'large'
-         });
-
-         });*/
-
 // 4. The API will call this function when the video player is ready.
-        /* function onPlayerReady(event) {
-         //event.target.playVideo();
-         player.loadVideoById({
-         'videoId': "g4mHPeMGTJM",
-         });
-         }*/
-
-
-        /*function onPlayerReady(event) {
-         ready = true;
-         console.log("Player:");
-         console.log(player);
-         }*/
         function onPlayerReady(event) {
             playerReady = true;
             event.target.playVideo();
@@ -151,13 +126,6 @@ jQuery(function ($) { // First argument is the jQuery object
                 goToNext();
             }
         }
-
-        /*function onPlayerStateChange(event) {
-         if (event.data == YT.PlayerState.PLAYING && !done) {
-         setTimeout(stopVideo, 6000);
-         done = true;
-         }
-         }*/
 
         function stopVideo() {
             player.stopVideo();
@@ -390,97 +358,4 @@ jQuery(function ($) { // First argument is the jQuery object
 
     /*end*/
 });
-
-//old stuff starts
-/*var substringMatcher = function (strs) {
- return function findMatches(q, cb) {
- var matches, substringRegex;
-
- // an array that will be populated with substring matches
- matches = [];
-
- // regex used to determine if a string contains the substring `q`
- substrRegex = new RegExp(q, 'i');
-
- // iterate through the pool of strings and for any string that
- // contains the substring `q`, add it to the `matches` array
- $.each(strs, function (i, str) {
- if (substrRegex.test(str)) {
- matches.push(str);
- }
- });
-
- cb(matches);
- };
- };*/
-
-
-/*function getTags() {
- var tags = [];
- $.ajax({
- type: "GET",
- url: "/getTags/",
- dataType: "json",
- complete: function (xhr, textStatus) {
- var oldTags = xhr.responseJSON;
- for (var x = 0; x < oldTags.length; x++) {
- tags.push(oldTags[x]);
- }
- }
- });
- return tags;
- };*/
-
-/*function getArtists() {
- var artists = [];
- $.ajax({
- type: "GET",
- url: "/getArtists/",
- dataType: "json",
- complete: function (xhr, textStatus) {
- var oldArtists = xhr.responseJSON;
- for (var x = 0; x < oldArtists.length; x++) {
- artists.push(oldArtists[x]);
- }
- }
- });
- return artists;
- };*/
-
-
-/*$("button#addTag").on("click", function () {
- var newTag = ($("#tagInput").val());
- var artistName = $("input#artistName").val();
- var data = {
- newTag: newTag,
- artistName: artistName
- };
-
- $.ajax({
- type: 'POST',
- url: window.location.href + 'tag/',
- data: JSON.stringify(data),
- dataType: 'json',
- contentType: 'application/json; charset=utf-8',
- success: function (msg) {
- //if duplicate tag
- if (msg.tag_id == -1) {
- $("#tag_error").remove();
- $('#tagInput').css({
- 'border': '2px solid red'
- });
- $("<div class='row text-center' id='tag_error'>Error:<b>'" + newTag + "'</b> Already exists</div>").insertBefore(".addTag .row .col-md-12 .row");
- }
- else {
- $('#artistTags').append($('<option>', {
- value: msg.tag_id,
- text: data.newTag
- }));//TODO: insert where it should be in abc order
- $('#artistTags').val(msg.tag_id);
- $('.addTag').modal('hide');
- }
- }
-
- })
- });*/
 
